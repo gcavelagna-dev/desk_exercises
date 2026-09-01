a898ac0483a359627fd9a1ef56de367e24071355
cavelagna <gcavelagna@gmail.com>
2026-09-01 16:55:31 -0300

    TestDesks

--- src/desk_exercise/aplication/TestDesks.java
+++ src/desk_exercise/aplication/TestDesks.java
@@ -11,6 +11,7 @@ public class TestDesks {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int amount = 0;
+
         while (true){
             System.out.println("How many tables will you have");
 
@@ -27,7 +28,39 @@ public class TestDesks {
                 continue;
             }
         }
+        System.out.println("----------------------------");
+        input.nextLine();
+        boolean hasDrawer;
+        for (int i = 0; i < amount; i++) {
+            System.out.println("\nWhat kind of wood is the table made of?");
+            String wood = input.nextLine();
 
+            System.out.println("\nWhat is the color?");
+            String color = input.nextLine();
+
+            while (true){
+                System.out.println("\nDoes the desk have drawers?[Y/N]");
+                String response = input.next();
+
+
+                if (response.equalsIgnoreCase("Y")){
+                    hasDrawer = true;
+                    break;
+                } else if (response.equalsIgnoreCase("N")){
+                    hasDrawer = false;
+                    break;
+                }else {
+                    System.err.println("\nUnidentified responses ");
+                    input.next();
+                    continue;
+                }
+            }
+
+            if (hasDrawer){
+                System.out.println("\n");
+            }
+
+        }
         Desk oak = new Desk("Oak", "Orange", true);
         Desk pine = new Desk("Pine", "Pink", false);
         Desk ash = new Desk("Ash", "Brown", false);
@@ -39,6 +72,11 @@ public class TestDesks {
         deskRegister.addDesk(ash);
 
         System.out.println("\n----------------------------\n");
+
+
+        //                  testar o showTables                \\
+
+
 //        deskRegister.showTables(deskRegister.showTables());
         System.out.println("\n----------------------------\n");
 
