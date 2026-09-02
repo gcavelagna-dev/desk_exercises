3f73829cca3d56836f227859562ba9990f3814f3
cavelagna <gcavelagna@gmail.com>
2026-09-02 18:34:44 -0300

    Main sketch

--- src/desk_exercise/aplication/Main.java
+++ src/desk_exercise/aplication/Main.java
@@ -1,4 +1,12 @@
 package poo.exercises.desk_exercise.aplication;
 
 public class Main {
+    public static void main(String[] args) {
+
+        /*
+        Create a system using a switch-case statement,
+        utilize classes, and try to avoid importing InvalidExceptions in the main method
+         */
+        
+    }
 }
