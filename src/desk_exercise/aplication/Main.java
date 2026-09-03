f4fdf18ac9b7535c19b9496b236a841d66fc6fb1
cavelagna <gcavelagna@gmail.com>
2026-09-03 16:46:52 -0300

    refactor: cleaning main

--- src/desk_exercise/aplication/Main.java
+++ src/desk_exercise/aplication/Main.java
@@ -1,33 +1,16 @@
 package poo.exercises.desk_exercise.aplication;
 
+import poo.exercises.desk_exercise.domain.DeskRegister;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+        DeskRegister deskRegister = new DeskRegister();
 
-        /*
-        Create a system using a switch-case statement,
-        utilize classes, and try to avoid importing InvalidExceptions in the main method
-         */
+        System.out.println("");
 
-        /*
-        Dica: Escolha um idioma (o inglês é o padrão do mercado) e foque em dizer o que o commit faz.
-        Em vez de "THE TEST WORKED", use algo como "test: assert desk creation with valid parameters".
-        2. Uso do Conventional CommitsMuitas empresas usam um padrão de prefixos para organizar o histórico.
-        Adotar isso desde o início vai destacar muito o seu perfil.
-        Veja como algumas das suas mensagens ficariam nesse padrão:
-        fix(desk): resolve ArrayList bug in constructortest
-        (desk): add exception handling unit testsstyle: fix spacings and code
-        formattingdocs: update README with execution steps💡
-         dica avançada para o futuro: Git SquashNo dia a dia,
-         quando fazemos muitos commits seguidos para ajustar a mesma coisa
-         (como os seus commits seguidos de Polishing texts),
-         os desenvolvedores costumam usar uma técnica chamada Squash.
-          Ela serve para "juntar" vários commits pequenos
-           em um único commit principal antes de enviar a versão final.
-        Assim, o histórico fica mais limpo e fácil de ler.
-         */
     }
 }
